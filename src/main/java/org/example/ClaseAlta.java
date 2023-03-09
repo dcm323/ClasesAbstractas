ea9b5b732a8277bcdc962a302278cc4827e58bb3
Andersonmuñoz <andersonm8031@gmail.com>
2023-03-09 10:21:04 -0500

    Se crea ejemplo Clase Abstracta

---
+++ src/main/java/org/example/ClaseAlta.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class ClaseAlta extends Sayayin{
+
+    public ClaseAlta() {
+    }
+
+    public ClaseAlta(String nombre, Integer poderPelea, Integer edad, String colorPelo) {
+        super(nombre, poderPelea, edad, colorPelo);
+    }
+
+    @Override
+    public void pelear() {
+        System.out.println(this.getNombre()+"Pelea con elegancia");
+    }
+}
---
+++ src/main/java/org/example/PlanetaSayayin.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class PlanetaSayayin {
+    public static void main(String[] args) {
+
+
+    //creando un objeto de clase alta
+    ClaseAlta guerrero1 = new ClaseAlta("Vegeta",90,30,"negro");
+    guerrero1.pelear();
+        //creando un objeto de clase alta
+    ClaseBaja guerrero2 = new ClaseBaja("Goku",80,25,"negro");
+    guerrero2.pelear();
+    }
+}
